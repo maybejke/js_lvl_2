c3361843660631d535f0d550cf7dbc92e3e547cc
AlexeyRakitin <amrakitin@gmail.com>
2020-02-24 16:28:28 +0300

    hm_lesson3_1_task

--- js/main_2.js
+++ js/main_2.js
@@ -1,8 +1,11 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+
 class ProductItem {
 	constructor (product, img='https://placehold.it/200x150') {
-		this.title = product.title;
+		this.title = product.product_name;
 		this.price = product.price;
-		this.id = product.id;
+		this.id = product.id_product;
 		this.img = img;
 	}
 
@@ -12,7 +15,12 @@ class ProductItem {
 					<div class='desc'>
 						<h3> ${this.title}</h3>
 						<p ${this.price}</p>
-						<button class="buy-btn"> Buy </button>
+						<button class="buy-btn"
+						data-id="${this.id}"
+						data-name="${this.name}"
+						data-price="${this.price}"
+						data-img="${this.img}"
+						> Buy </button>
 					</div>
 				</div>`	
 	} 
@@ -27,25 +35,27 @@ class ProductList {
 		// для работы со всеми продуктами
 		this.allProducts = [];
 		// забираем данные с сервера
-		this._fetchProducts();
-		// отрисовываем продукты
-		this.render();
-		console.log(this.goods);
-    	console.log(this.allProducts);
-    	console.log(this.sum());
-
+		this._getProducts()
+			// разбираем еще раз полученные данные
+			.then(data => {
+				this.goods = [...data];
+				// отрисовываем продукты
+				this.render();
+			});
 	}
 
 	// метод забора данных из базы
-	_fetchProducts() {
-		this.goods = [
-			{id: 1, title: 'MacBook Air', price: 1000},
-			{id: 2, title: 'MacBook Pro 13', price: 1200},
-			{id: 3, title: 'MacBook Pro 15', price: 1300},
-			{id: 4, title: 'MacBook Air 2018', price: 700},
-			{id: 5, title: 'MacBook Pro 16', price: 1500},
-		];
+	_getProducts() {
+		// забираем данные из файла гитхаба
+		return fetch(`${API}/catalogData.json`)
+		// забираем результат, парсинг json
+		.then(result => result.json())
+		// отлавливаем ошибки
+		.catch(error => {
+			console.log('error', error);
+		});
 	}
+
 	// генерируем шаблон
 	render() {
 		const block = document.querySelector(this.container);
@@ -60,13 +70,35 @@ class ProductList {
 		}
 	}
 
-	sum() {
-		let totalPrice = 0;
-		for (let product of this.allProducts) {
-			totalPrice += product.price; 
-		}
-		return totalPrice;
+	calcTotal() {
+		// в продуктах, accum - общая сумма, item.price - цена одного продукта, ", 0" - стратовая.
+		return this.allProducts.reduce((accum, item) => accum += item.price, 0)
 	}
 }
+
+// class Catalog extends ProductList {
+// 	constructor (cart, url = '/catalogData.json', container='.products') {
+// 		super (url, container)
+// 		this.cart = cart;
+// 	}
+
+// 	_addListeners () {
+// 		document.querySelector('.products').addEventListener('click', (evt) => {
+// 			if (evt.target.classList.contains('buy-btn')) {
+// 				this.cart.addProduct(evt.target);
+// 			}
+// 		})
+// 	}
+// 	_init() {
+// 		this._getProducts().then(data => {
+// 				this.goods = [...data];
+// 				// отрисовываем продукты
+// 				this.render();
+// 				.finally (() => {this.addListeners ()})
+// 			});
+// 	}
+// }
+
+// class BasketItem extends ProductItem {}
 
 new ProductList();
